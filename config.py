37d4b723a9fe842d6f538528a8173d445092247c
ArteChudS <64978629+ArteChudS@users.noreply.github.com>
2025-05-20 17:45:02 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+protected_path = "C:\\Users\\Artemy\\source\\repos\\FaceRecognition\\FaceRecognition\\ne trogatj"
+ref_image_path = "owner.png"
+cascade_path="haarcascade_frontalface_default.xml"
+distance_threshold=0.50
+camera_id= 1 # 0 - default camera, 1 - external camera
